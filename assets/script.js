958e967572efe248da80bf9eb601e174e1eb30fd
Benjamin Sprenger <115416353+azrazel95@users.noreply.github.com>
2023-03-09 00:08:53 -0400

    finally got the hide functionality to workgit add -A!

--- assets/script.js
+++ assets/script.js
@@ -1,28 +1,80 @@
-var card1 = document.getElementById("card1");
-var card2 = document.getElementById("card2");
-var card3 = document.getElementById("card3");
-var card4 = document.getElementById("card4");
-var card5 = document.getElementById("card5");
-var nextBtn = document.querySelector(".next-btn");
-var cards = [card1, card2, card3, card4, card5];
+var card1 = document.getElementById("card0");
+var card2 = document.getElementById("card1");
+var card3 = document.getElementById("card2");
+var card4 = document.getElementById("card3");
+var finishcard = document.querySelector('#finishcard')
+var startcard = document.querySelector('#startcard')
+var Btn = document.querySelectorAll(`.btn`);
+var cards = [card0, card1, card2, card3];
 let i = 0;
+let message;
 var quizDuration = 60; 
 var timePenalty = 10; 
 var currentQuestion = 0;
 var score = 0;
 var timerIntervalId;
 
+function initGame(){
+    startcard.setAttribute('data-state', 'hidden');
+    card1.setAttribute('data-state', 'visible');
+    startTimer();
+    displayQuestion();
+}
+
+function displayQuestion() {
+    // Display question based on currentQuestion index
+}
+
 function showNextCard() {
-  cards[i].setAttribute("data-state", "hidden");
-    i++;
-  cards[i].setAttribute('data-state', 'visible');
-  console.log(i, cards[i])
+    if(i < 3){
+        cards[i].setAttribute("data-state", "hidden");
+        i++;
+        cards[i].setAttribute('data-state', 'visible');
+        displayQuestion();
+    } else {
+        endGame();
+    }
 };
-nextBtn.addEventListener('click', showNextCard());
 
+function endGame() {
+    clearInterval(timerIntervalId);
+    finishcard.setAttribute('data-state', 'visible');
+    var displaymessage = document.getElementById('message');
+    displaymessage.innerHTML = `Good job! Your score is ${score}. Please enter your name so we may record your score!`;
+}
 
-//shownextcard
-//every time the function starts, 
+function startTimer() {
+    var timeLeft = quizDuration;
+    var timerDisplay = document.getElementById('timer');
 
+    timerIntervalId = setInterval(function() {
+        timeLeft--;
+        timerDisplay.textContent = timeLeft;
 
-   
+        if (timeLeft <= 0) {
+            endGame();
+        }
+    }, 1000);
+}
+
+function handleAnswer(answer) {
+    if (answer === currentQuestion.answer) {
+        score++;
+        // Display message for correct answer
+    } else {
+        quizDuration -= timePenalty;
+        // Display message for incorrect answer
+    }
+    showNextCard();
+}
+
+function initButtons() {
+    Btn.forEach(function(button) {
+        button.addEventListener("click", function() {
+            handleAnswer(this.getAttribute("data-value"));
+        });
+    });
+}
+
+initGame();
+initButtons();
