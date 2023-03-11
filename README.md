0c7f9990ac3eaaa8f996c060dbc794b92d5b2f0e
Benjamin Sprenger <115416353+azrazel95@users.noreply.github.com>
2023-03-10 23:59:09 -0500

    made css prettier, reconfigured readme

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # riddling
-a quiz using web-api's
+  
+
+- [Installation](#installation)
+
+- [Usage](#usage)
+
+- [Contribution](#contributing)
+
+- [License](#license)
+
+
+## License
+
+![MIT](https://img.shields.io/github/license/azrazel95/riddling)
+
+
+a quiz webapp!
+
+![ page on first startup displaying a quiz starting card](./assets/riddling1.png "page on load") 
+
+
+
+## Installation
+
+simply open the website in the browser!
+
+
+
+## Usage
+
+enter the website, enjoy!
+![ page displaying a question card](./assets/riddling2.png "quiz started!") 
+
+
+## Contributing
+
+please do not contribute
+
+
+
+## Testing
+
+click on every element whenever you like
+
+![ page displaying a score card](./assets/riddling3.png "highscores") 
+
+## Questions
+
+Contact me!
+
+Github: https://github.com/azrazel95
+
+Email: beni.sprenger@hotmail.com
+
--- assets/script.js
+++ assets/script.js
@@ -1,315 +1,235 @@
+//defining my questions at the beginning
+
 const questions = [
-    {
-      question: "What does hoisting refer to?",
-      choices: [
-        "JavaScript pushing all variables to the top",
-        "Javascript uplifting CSS",
-        "Javascript being a primarily naval based language"
-      ],
-      answer: "JavaScript pushing all variables to the top"
-    },
-    {
-        question: "What is css used for?",
-        choices: [
-          "Decorating!",
-          "Making the code cascade",
-          "Using style sheets to riverraft"
-        ],
-        answer: "Decorating!"
-    },
-    {
-        question: "What does HTML stand for?",
-        choices: [
-          "Hyped Tempurpedic Monkey Lions",
-          "Hyper Text Markup Language",
-          "High Tonic Music Listeners"
-        ],
-        answer: "Hyper Text Markup Language"
-    },
-    {
-        question: "What are for loops usually used for?",
-      choices: [
-        "JavaScript pushing all variables to the top",
-        "they are javascripts answer to fruit loops",
-        "selecting an integer to increase for use in a function"
-      ],
-      answer: "selecting an integer to increase for use in a function"
-    }
-  ];
-  
-  // Define other constants and variables you need
-  const startButton = document.querySelector("#start-button");
-  const timerElement = document.querySelector("#timer");
-  const questionEl = document.querySelector("#card0");
-  const questionheader = document.querySelector('#questionheader')
-  const choicesElement = document.querySelector("#choices");
-  const scorebtn = document.querySelector("#scores");
-  const questionbtn = document.querySelector('#btn1')
-  let highScores = document.querySelector('#scoreCard')
-  let scoreEl = document.querySelector('#scoreEl')
-  let li = document.createElement("li");
-  let form = document.createElement('form');
-  let currentQuestionIndex = 0;
-  let timeLeft = 30;
-  let score = 0;
-  let timerIntervalId;
-  let scores = []
-  let userInits
-  let scoreNum
-  function setUpScores(scores){
-    // localStorage.setitem(('scores', JSON.stringify(scores))
-    if (localStorage.getItem('scores') !== null) {
-        scores = localStorage.getItem('scores');
-    } else {
-        localStorage.setItem("scores", JSON.stringify(scores));
-    }
+  {
+    question: "What does hoisting refer to?",
+    choices: [
+      "JavaScript pushing all variables to the top",
+      "Javascript uplifting CSS",
+      "Javascript being a primarily naval based language"
+    ],
+    answer: "JavaScript pushing all variables to the top"
+  },
+  {
+    question: "What is css used for?",
+    choices: [
+      "Decorating!",
+      "Making the code cascade",
+      "Using style sheets to riverraft"
+    ],
+    answer: "Decorating!"
+  },
+  {
+    question: "What does HTML stand for?",
+    choices: [
+      "Hyped Tempurpedic Monkey Lions",
+      "Hyper Text Markup Language",
+      "High Tonic Music Listeners"
+    ],
+    answer: "Hyper Text Markup Language"
+  },
+  {
+    question: "What are for loops usually used for?",
+    choices: [
+      "to create ongoing css cascades",
+      "they are javascripts answer to fruit loops",
+      "selecting an integer to increase for use in a function"
+    ],
+    answer: "selecting an integer to increase for use in a function"
   }
-  
-  // Define functions you need
-  function initGame() {
-    // Hide the start button and show the first question
-    startcard.setAttribute('data-state', 'hidden');
-    questionEl.setAttribute('data-state', 'visible');
-    showQuestion();
-    // Start the timer
-    timerIntervalId = setInterval(updateTimer, 1000);
+];
+
+// defining my document variables
+const startButton = document.querySelector("#start-button");
+const timerElement = document.querySelector("#timer");
+const questionEl = document.querySelector("#card0");
+const questionheader = document.querySelector('#questionheader')
+const choicesElement = document.querySelector("#choices");
+const scorebtn = document.querySelector("#scores");
+const questionbtn = document.querySelector('#btn1')
+let highScores = document.querySelector('#scoreCard')
+let scoreEl = document.querySelector('#scoreEl')
+let li = document.createElement("li");
+let form = document.createElement('form');
+//declaring function results and variables used amongst multiple functions
+let currentQuestionIndex = 0;
+let timeLeft = 30;
+let score = 0;
+let timerIntervalId;
+//also an array
+let scores = []
+let userInits
+let scoreNum
+let scoreInput;
+//setting up scores at beginning of pageload, in case you come back to the webpage later and want to view your old scores
+//also setting up the localstorage entry in case it does not yet exist
+function setUpScores(scores) {
+  if (localStorage.getItem('scores') !== null) {
+    scores = localStorage.getItem('scores');
+  } else {
+    localStorage.setItem("scores", JSON.stringify(scores));
   }
-  //resetTimer upon gameover
-  function showQuestion() {
-    // Display the current question and choices
-    const currentQuestion = questions[currentQuestionIndex];
-    questionheader.textContent = currentQuestion.question;
-    choicesElement.innerHTML = ""; // Clear previous choices
-    currentQuestion.choices.forEach((choice) => {
-        let li = document.createElement("li");
-        li.classList.add("list-group-item", "px-4")
-      const label = document.createElement("label");
-      const radio = document.createElement("input");
-      radio.type = "radio";
-      radio.name = "answer";
-      radio.value = choice;
-      label.appendChild(radio);
-      label.appendChild(document.createTextNode(choice));
-      li.appendChild(label);
-      choicesElement.appendChild(li);
-      questionbtn.addEventListener("click", checkAnswer);
-      console.log(scores)
-    });
-  }
-  
-  function updateTimer() {
-    // Update the timer and check if time is up
-    timeLeft--;
-    timerElement.textContent = timeLeft;
-    if (timeLeft <= 0) {
-      gameOver();
-    }
-  }
-  
-  function checkAnswer() {
-    // Check if the selected answer is correct
-    const selectedAnswer = document.querySelector("input[name='answer']:checked");
-    if (!selectedAnswer) {
-      return; // No answer selected
-    }
-    const currentQuestion = questions[currentQuestionIndex];
-    if (selectedAnswer.value === currentQuestion.answer) {
-      score++;
-    } else {
-      timeLeft -= 10; // Subtract 10 seconds for wrong answer
-      if (timeLeft < 0) {
-        timeLeft = 0;
-      }
-      timerElement.textContent = timeLeft;
-    }
-    // Move on to the next question or end the game
-    currentQuestionIndex++;
-    console.log(score)
-    if (currentQuestionIndex >= questions.length) {
-      gameOver();
-    } else {
-      showQuestion();
-    }
-  }
-  
-  let scoreInput;
-  function gameOver() {
-    // Stop the timer and display the final score
-    clearInterval(timerIntervalId);
-    
-  
-    
-    questionheader.classList.add("text-center", "font-weight-bold")
-    questionheader.textContent = "Game Over";
-    choicesElement.classList.add("text-center")
-    choicesElement.textContent = "Your score:";
+}
+
+// executes on click of the start button
+function initGame() {
+  // Hides the starting card and shows the question card
+  startcard.setAttribute('data-state', 'hidden');
+  questionEl.setAttribute('data-state', 'visible');
+  //populates the question card with the first question
+  showQuestion();
+  // Starts the timer
+  timerIntervalId = setInterval(updateTimer, 1000);
+}
+function showQuestion() {
+  // Displays the current question and choices
+  const currentQuestion = questions[currentQuestionIndex];
+  questionheader.textContent = currentQuestion.question;
+  // Clears previous choices
+  choicesElement.innerHTML = "";
+  //iterates over the choices and displays them on the card
+  currentQuestion.choices.forEach((choice) => {
+    //i had to declare li individually for the functions because otherwise it didnt populate right
+    let li = document.createElement("li");
+    //makes it pretty
+    li.classList.add("list-group-item", "px-4")
+    //using radio ensures you can only choose one choice at a time
+    const label = document.createElement("label");
+    const radio = document.createElement("input");
+    radio.type = "radio";
+    radio.name = "answer";
+    //checks whether your choice is right or wrong
+    radio.value = choice;
+    //appends
+    label.appendChild(radio);
+    label.appendChild(document.createTextNode(choice));
+    li.appendChild(label);
     choicesElement.appendChild(li);
-    choicesElement.appendChild(form)
-    
-    console.log(form)
-    li.classList.add("list-group-item", "px-4", "text-center")
-    li.setAttribute("id", "userScore")
-    li.innerHTML = score;
-    form.innerhtml = ""
-    form.classList.add('form-inline', "m-2");
-    const scoreInput = document.createElement('input');
-    let remaininginput = form.querySelector("#scoreInput");
-    if (remaininginput){
-      remaininginput.remove()
+    //ensures that the score is added up
+    questionbtn.addEventListener("click", checkAnswer);
+  });
+}
+
+function updateTimer() {
+  // Updates the timer and checks if time is up
+  timeLeft--;
+  timerElement.textContent = timeLeft;
+  //if the time is up, executes gameoverfunction
+  if (timeLeft <= 0) {
+    gameOver();
+  }
+}
+
+function checkAnswer() {
+  // Checks if the selected answer is correct
+  const selectedAnswer = document.querySelector("input[name='answer']:checked");
+  //if no answer is selected, it will reject
+  if (!selectedAnswer) {
+    return;
+  }
+  //checks the selected answer against the correct answer to the current question. if not, detracts 10 seconds from the timer
+  const currentQuestion = questions[currentQuestionIndex];
+  if (selectedAnswer.value === currentQuestion.answer) {
+    score++;
+  } else {
+    timeLeft -= 10;
+    //ensures the timer doesnt go below 0
+    if (timeLeft < 0) {
+      timeLeft = 0;
     }
-    scoreInput.id = 'scoreInput';
-    scoreInput.classList.add('form-control', 'mr-sm-2');
-    scoreInput.type = "text"
-    scoreInput.placeholder = "enter your Initals!";
-    scoreInput.setAttribute("aria-label", "score");
-    form.appendChild(scoreInput)
-    questionbtn.removeEventListener('click', saveScore);
-    questionbtn.addEventListener('click', saveScore);
-
-    
-    // TODO: Allow user to save initials and score
-    
+    //populates/updates the timer element
+    timerElement.textContent = timeLeft;
   }
-  function scoreCard(){
-    clearInterval(timerIntervalId);
-    scores = JSON.parse(localStorage.getItem('scores')) || []
-    startcard.setAttribute('data-state', 'hidden');
-    questionEl.setAttribute('data-state', 'hidden');
-    highScores.setAttribute('data-state', 'visible');
-    console.log(scores)
-    let remainingscoreEl = scoreEl.querySelectorAll(".list-group-item")
-    if (remainingscoreEl){
-      remainingscoreEl.forEach(element => element.remove())
-    }
-    for (let i = 0; i < scores.length; i++) {
-      const li = document.createElement('li');
-      li.classList.add('list-group-item', 'px-4')
-      li.innerText = `${scores[i].initials}: ${scores[i].score}`;
-      scoreEl.append(li);
-  }}
-  function saveScore(){
-    console.log(scores)
-    
-    
-    userInits = document.getElementById('scoreInput').value
-    scoreNum = document.getElementById("userScore").innerText
-    let scoreObject = { initials: userInits, score: scoreNum }
-    scores.push(scoreObject);
-    localStorage.setItem("scores", JSON.stringify(scores))
-    console.log(scores)
-    questionbtn.removeEventListener('click', saveScore);
-    scoreCard()
+  // Moves on to the next question or ends the game
+  currentQuestionIndex++;
+  if (currentQuestionIndex >= questions.length) {
+    gameOver();
+  } else {
+    showQuestion();
   }
-  function backtostart(){
-    highScores.setAttribute('data-state', 'hidden');
-    startcard.setAttribute('data-state', 'visible');
-    currentQuestionIndex = 0;
-    timeLeft = 30
-    score = 0
+}
+
+
+function gameOver() {
+  // Stops the timer
+  clearInterval(timerIntervalId);
+
+
+  //populates the card for the game over event
+  questionheader.classList.add("text-center", "font-weight-bold")
+  questionheader.textContent = "Game Over";
+  choicesElement.classList.add("text-center")
+  choicesElement.textContent = "Your score:";
+  choicesElement.appendChild(li);
+  // creates a form to enter your initials
+  choicesElement.appendChild(form)
+  li.classList.add("list-group-item", "px-4", "text-center")
+  li.setAttribute("id", "userScore")
+  //shows your score
+  li.innerHTML = score;
+  form.classList.add('form-inline', "m-2");
+  const scoreInput = document.createElement('input');
+  //clears the form so it doesnt show the input multiple times
+  let remaininginput = form.querySelector("#scoreInput");
+  if (remaininginput) {
+    remaininginput.remove()
   }
-  setUpScores();
-  // Attach event listeners
-// var card1 = document.getElementById("card0");
-// var card2 = document.getElementById("card1");
-// var card3 = document.getElementById("card2");
-// var card4 = document.getElementById("card3");
-// var answer1 = document.getElementById('correctanswer1');
-// var answer2 = document.getElementById('correctanswer2');
-// var answer3 = document.getElementById('correctanswer3');
-// var answer4 = document.getElementById('correctanswer4');
-// var wrong1 = document.getElementById('wrong1');
-// var wrong2 = document.getElementById('wrong2');
-// var wrong3 = document.getElementById('wrong3');
-// var wrong4 = document.getElementById('wrong4');
-// var wrongs1 = document.getElementById('wrongs1');
-// var wrongs2 = document.getElementById('wrongs2');
-// var wrongs3 = document.getElementById('wrongs3');
-// var wrongs4 = document.getElementById('wrongs4');
-// var wronganswers = [wrong1, wrongs1, wrong2, wrongs2, wrong3, wrongs3, wrong4, wrongs4]
-// var rightanswers = [answer1, answer2, answer3, answer4 ];
-// var finishcard = document.querySelector('#finishcard');
-// var startcard = document.querySelector('#startcard');
-// var Btn = document.querySelectorAll(`.btn`);
-// var cards = [card1, card2, card3, card4];
-// let i = 0;
-// let message;
-// let timeLeft = 30; 
-// var timePenalty = 10; 
-// var currentQuestion = 0;
-// let score = 0;
-// var timerIntervalId;
+  scoreInput.id = 'scoreInput';
+  scoreInput.classList.add('form-control', 'mr-sm-2');
+  scoreInput.type = "text"
+  scoreInput.placeholder = "enter your Initals!";
+  scoreInput.setAttribute("aria-label", "score");
+  form.appendChild(scoreInput)
+  // i had to remove it at first in case it was still there from the first time around so it doesnt execute twice(this was a nightmare to debug)
+  questionbtn.removeEventListener('click', saveScore);
+  questionbtn.addEventListener('click', saveScore);
+}
 
-// function initGame(){
-//     startcard.setAttribute('data-state', 'hidden');
-//     card1.setAttribute('data-state', 'visible');
-//     startTimer();
-// }
-
-
-// // function generateCards{
-// //
-// //}
-
-// function showNextCard() {
-//     var selectedAnswer = document.querySelector('input[name="answer"]:checked');
-    
-//     console.log(selectedAnswer)
-//     handleAnswer(selectedAnswer)
-//     if(i < 3){
-//         cards[i].setAttribute("data-state", "hidden");
-//         i++;
-//         cards[i].setAttribute('data-state', 'visible');
-//         // displayQuestion();
-//         console.log(i)
-//     } else {
-//         cards[i].setAttribute("data-state", "hidden");
-//         endGame();
-//         console.log(i)
-//     }
-// };
-
-// function endGame() {
-//     clearInterval(timerIntervalId);
-//     cards[i].setAttribute('data-state', 'hidden')
-//     finishcard.setAttribute('data-state', 'visible');
-//     var displaymessage = document.getElementById('message');
-//     displaymessage.innerHTML = `Good job! Your score is ${score}. Please enter your name so we may record your score!`;
-// }
-
-// function startTimer() {
-    
-//     var timerDisplay = document.getElementById('timer');
-
-//     timerIntervalId = setInterval(function() {
-//         timeLeft--;
-//         timerDisplay.textContent = timeLeft;
-
-//         if (timeLeft <= 0) {
-//             endGame();
-//         }
-//     }, 1000);
-// }
-
-// function handleAnswer(selectedAnswer) {
-//     if (rightanswers.includes(selectedAnswer)) {
-//         score++;
-//         // Display message for correct answer
-//     } else if(wronganswers.includes(selectedAnswer)) {
-//         timeLeft - timePenalty;
-//         console.log(timeLeft, timePenalty)
-//         // Display message for incorrect answer
-//     }
-//     console.log(timeLeft, timePenalty, score)
-// }
-
-
-
-// //everytime this element is hit, increment score by 1
-// //onclick='increment"
-// //onclick='punish
-// //its not an onclick, its a onselect
-
-// //you shouldnt have to do it all by itself, DONT REPEAT YOURSELF
-// //THEREFORE everytime that id is selected, increment by 1
-// //everytime the other is selected, punish
-// //if answerELId =1 score++
+function scoreCard() {
+  //ensures that if pressed during the quiz you dont keep the timer running for quality of life
+  clearInterval(timerIntervalId);
+  //gets the scores from localstorage if not previously populated locally
+  scores = JSON.parse(localStorage.getItem('scores')) || []
+  //hides the other 2 cards in case the button is pressed at any moment
+  startcard.setAttribute('data-state', 'hidden');
+  questionEl.setAttribute('data-state', 'hidden');
+  highScores.setAttribute('data-state', 'visible');
+  //clears the score so it doesnt populate twice (again, nightmare to debug)
+  let remainingscoreEl = scoreEl.querySelectorAll(".list-group-item")
+  if (remainingscoreEl) {
+    remainingscoreEl.forEach(element => element.remove())
+  }
+  //iterates over the scores array to create a list item for every score
+  for (let i = 0; i < scores.length; i++) {
+    const li = document.createElement('li');
+    li.classList.add('list-group-item', 'px-4')
+    li.innerText = `${scores[i].initials}: ${scores[i].score}`;
+    scoreEl.append(li);
+  }
+}
+function saveScore() {
+  //pushes the scores and initials to scores, then puts them in localstorage
+  userInits = document.getElementById('scoreInput').value
+  scoreNum = document.getElementById("userScore").innerText
+  let scoreObject = { initials: userInits, score: scoreNum }
+  scores.push(scoreObject);
+  localStorage.setItem("scores", JSON.stringify(scores))
+  //removes savescore from the button.
+  // in hindsight it would be much leaner to make javascript generate the cards individually,
+  // but i really wanted to make use of the data-state hidden functionality to see if i could make it work.
+  // unfortunately that led to the need for this.
+  questionbtn.removeEventListener('click', saveScore);
+  //displays all scores at the end of the game
+  scoreCard()
+}
+function backtostart() {
+  //on buttonpress of the button on highscores, it will return you to the starting card
+  highScores.setAttribute('data-state', 'hidden');
+  startcard.setAttribute('data-state', 'visible');
+  //resets to beginningquestion, resets the timer, and the score for a new game
+  currentQuestionIndex = 0;
+  timeLeft = 30
+  score = 0
+}
+//grabs the scores array for page startup
+setUpScores();
